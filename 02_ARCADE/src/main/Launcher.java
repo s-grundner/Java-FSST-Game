2d1d110d3b06b2927e43b6cbcad4c9a8606bd745
s-grundner <simon.grundner@gmail.com>
2021-01-14 08:02:06 +0100

    project refactor

--- 02_ARCADE/src/main/Launcher.java
+++ 02_ARCADE/src/main/Launcher.java
@@ -1,18 +1,13 @@
 package main;
 
 /**
- * @author	Simon Grundner <br>
- *			3AHEL
- */
-
-/**
  * @author Simon Grundner
  *		   3AHEL
  */
+
 public class Launcher {
 
 	public static void main(String[] args) {
 		new Thread(new GameLoop(new Game())).start();
 	}
-
 }
